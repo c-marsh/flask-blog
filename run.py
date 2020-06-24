99bd76eeb0097cdf6ce822016a7cef8ffc992fb3
Chris Marsh <cmhilfiker@gmail.com>
2020-06-24 12:19:38 +0000

    Added about/hoem page routing
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- run.py
+++ run.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "<h1>Hello World</h1>"
+@app.route("/home")
+def home():
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')),
