60ed2cd55d134c4f46e3d0e78583da2da1079013
Chris Marsh <cmhilfiker@gmail.com>
2020-06-24 12:33:45 +0000

    Add basic blog data on home page
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+posts = [
+    {"author": "John Smith",
+     "title": "Post 1",
+     "content": "Post 1 contents",
+     "date": "21/2/2020"},
+    {"author": "Jane Doe",
+     "title": "Post 2",
+     "content": "Post 2 contents",
+     "date": "22/2/2020"}
+
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html")
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')),
